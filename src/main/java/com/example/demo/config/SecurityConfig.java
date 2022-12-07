8807737634395253acab8f7a502495b5b1654f53
yoshikawa <hiyoko.9445250@gmail.com>
2022-12-07 14:35:11 +0900

    error

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
             )
             .formLogin(login -> login
                 .loginPage("/login")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/calendars")
                 .permitAll()
             )
             .logout(logout -> logout
--- src/main/resources/static/js/calendars.js
+++
@@ -1,8 +0,0 @@
-/* ES6 module in Node.js environment */
-import Calendar from '@toast-ui/calendar';
-
-/* CommonJS in Node.js environment */
-const Calendar = require('@toast-ui/calendar');
-
-/* in the browser environment namespace */
-const Calendar = tui.Calendar;
